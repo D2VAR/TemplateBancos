24ac59287834c978aa34e393847e20754b55fe7c
DaianeOliveiraa <daianeldeoliveira@gmail.com>
2022-12-19 22:16:34 -0300

    consumer RecebedorPixConsumer - ajuste service

--- src/main/java/br/com/banco/domain/service/ContaService.java
+++ src/main/java/br/com/banco/domain/service/ContaService.java
@@ -3,9 +3,11 @@ package br.com.banco.domain.service;
 import br.com.banco.adapter.out.db.repository.ContaRepository;
 import br.com.banco.domain.dto.ContaRequest;
 import br.com.banco.domain.dto.ContaResponse;
+import br.com.banco.domain.dto.RecebedorPixMensagem;
 import br.com.banco.domain.exceptions.ContaNotFoundException;
 import br.com.banco.domain.exceptions.SaldoInsuficienteException;
 import br.com.banco.domain.model.Conta;
+import br.com.banco.port.in.RecebedorPixInputPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class ContaService {
+public class ContaService implements RecebedorPixInputPort {
     private final ContaRepository contaRepository;
     private final UsuarioService usuarioService;
 
@@ -86,11 +88,19 @@ public class ContaService {
 
     }
 
-    public void creditaConta(UUID id, BigDecimal valor) {
+    public void creditarConta(UUID id, BigDecimal valor) {
         Conta conta = getContaById(id);
         BigDecimal saldo = conta.getSaldo().add(valor);
         updateSaldo(id, saldo);
     }
 
+
+    @Override
+    public void validarAlteracaoSaldo(RecebedorPixMensagem recebedorPixMensagem) {
+        if (recebedorPixMensagem.getPixRealizado())
+            creditarConta(UUID.fromString(recebedorPixMensagem.getTransactionId()), new BigDecimal(recebedorPixMensagem.getValorTransferencia()));
+        debitarConta(UUID.fromString(recebedorPixMensagem.getTransactionId()), new BigDecimal(recebedorPixMensagem.getValorTransferencia()));
+
+    }
 
 }
